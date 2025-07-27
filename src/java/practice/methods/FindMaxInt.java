2baaa46ac7b5c0bca05372fda0b44d623614d412
Rohan Kale <rohankale67@gmail.com>
2025-07-27 21:54:39 +0100

    repository for all Java Practice

---
+++ src/java/practice/methods/FindMaxInt.java
@@ -0,0 +1,23 @@
+package java.practice.methods;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindMaxInt {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please, enter integer numbers separated by space: ");
+	
+	 
+	
+		System.out.println("*** Initial Array ***");
+		System.out.println(Arrays.toString(intArray));
+		System.out.println("*** Max number in array ***");
+		System.out.println(maxInt);
+	}
+
+	
+	public static int findMaxIntInArray(int[] intArray) {
+		<write your code here>
+	}
+}
