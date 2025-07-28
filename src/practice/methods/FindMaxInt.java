853ff07a55ecf7b900ad335683333c10e0cde9ff
Rohan Kale <rohankale67@gmail.com>
2025-07-28 14:25:57 +0100

    java practice methods first question

--- src/practice/methods/FindMaxInt.java
+++ src/practice/methods/FindMaxInt.java
@@ -1,14 +1,74 @@
 package practice.methods;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class FindMaxInt {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Please, enter integer numbers separated by space: ");
-	
-	 
+		
+		List<String> result = new ArrayList<>();
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		while (scan.hasNext()) {
+			
+		    result.add(scan.nextLine().trim());
+		    //numbers.add(Integer.parseInt(scan.nextLine().trim()));
+		   
+		    
+		  
+		    
+		    for (String num : result) {
+		    	System.out.println(num);
+		    	String[] split = num.split(" ");
+		    	int[] nums = new int[split.length];
+		    	//System.out.println(split.length); 
+		    	for (int i = 0; i < split.length; i++) {
+		            nums[i] = Integer.parseInt(split[i]);
+		            
+		          
+		    	}
+		    	
+		    	for (int integer: nums) {
+		    		System.out.print(integer);
+		    	}
+		    	 
+		    	
+		    	
+		    }
+		    
+		}
+		   
+				 
+				 
+			/*	 
+		    for (String num : result) {
+					  Integer neki = Integer.parseInt(num);
+					  
+					  numbers.add(neki); 
+					  System.out.print(numbers); 
+			            
+			        }
+				*/  
+			
+		
+		
+		
+		
+		/*
+		
+		double[] arr = Arrays.stream(scan.nextLine()
+                .trim()
+                .split(" "))
+                .filter(x -> !x.equals(""))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+          for(double each: arr){
+        	  System.out.print(each + "  ");
+        	  }
+	    */
 	
 		System.out.println("*** Initial Array ***");
 		//System.out.println(Arrays.toString(intArray));
@@ -20,4 +80,6 @@ public class FindMaxInt {
 	public static int findMaxIntInArray(int[] intArray) {
 		return 5; 
 	}
+	
+	
 }
