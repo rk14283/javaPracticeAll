0e2796ded47759222cd65adb3875a2292438248e
Rohan Kale <rohankale67@gmail.com>
2025-07-30 17:37:41 +0100

    matrix solving

--- src/practice/methods/RotateMatrix.java
+++ src/practice/methods/RotateMatrix.java
@@ -43,7 +43,8 @@ public class RotateMatrix {
 	private static void printMatrixToConsole(double[][] matrix) {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix.length; j++) {
-				System.out.print(matrix[i][j] + "\t");
+				//Print matrix to console
+				//System.out.print(matrix[i][j] + "\t");
 			}
 			System.out.println();
 		}
@@ -72,10 +73,80 @@ public class RotateMatrix {
 	
 	public static void rotate90(double[][] matrix) {
 		
+		
+		double matrixValueTestSix =0.0;
+
+		double[] mylistMain = new double[5];
+		 double[] mylist = new double[4]; // initialise array to have two elements
+	
+		System.out.println("matrix from 90 degree method"); 	
+		for (int i=0; i<matrix.length; i++) {
+			
+			for(int j=0; j<matrix.length; j++) {
+				//Row 1
+				 matrixValueTestSix =matrix[j][i];
+			    
+				 mylistMain[j] = matrix[j][i]; 
+				
+				 for(int k=0; k<(matrix.length-1); k++) {
+					  mylist[k] = matrix[k][i]; 
+					 
+				 }
+				 
+			}
+				  System.out.println(mylistMain[4]+"\t"+ mylist[3]+"\t"+mylist[2]+"\t"
+							+mylist[1]+ "\t"+ mylist[0]);
+			
+		}
+		
+		
 	}
+	
 
 	public static void rotate180(double[][] matrix) {
 		
+		
+		double matrixValueTestSix =0.0;
+		 double matrixValueTestSeven = 0.0; 
+		double[] myListMain = new double[5];
+		 double[] mylist = new double[6]; // initialise array to have two elements
+	
+		System.out.println("matrix from 180 degree method"); 	
+		
+		
+		
+		for(int l=matrix.length-1; l>=0; l--) {
+			
+			 
+			 //System.out.println(matrix[l][4]); 
+			 
+			 for(int m= matrix.length-1; m>=0; m--) {
+				 
+				// System.out.println(matrix[l][m]); 
+				 //All the elements 
+				 myListMain[m] = matrix[l][m]; 
+				 
+				 for(int n =0; n<=(matrix.length-1); n++) {
+					 
+					 mylist[n] = matrix[l][n]; 
+				 }
+				 
+			 }
+			 
+			 
+			 
+			 System.out.println(myListMain[4]+"\t"+mylist[3]+"\t"
+			 +mylist[2]+"\t"+mylist[1]+"\t"+mylist[0]); 
+			 
+			 
+			 
+			
+			 
+		 }
+		 
+		
+		
+			 
 	}
 
 	public static void rotate270(double[][] matrix) {
